9226c9265d93e19f0d2742686a9331891e8338fd
Jeremy <jermitesh20@gmail.com>
2023-09-28 04:21:42 +0300

    final commit

--- components/EsxAcademy/Menu.tsx
+++ components/EsxAcademy/Menu.tsx
@@ -76,7 +76,7 @@ const Menu = ({ items, step, setStep, title, returnHome, children }: any) => {
                 viewBox="0 0 24 24"
                 strokeWidth={1.5}
                 stroke="currentColor"
-                className="w-8 h-8 mt-1.5"
+                className="w-8 h-8"
               >
                 <path
                   strokeLinecap="round"
